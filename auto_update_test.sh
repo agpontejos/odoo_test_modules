23a5e6601fc000b4aadb15ebd2627b828e89c225
agpontejos <agpontejos@gmail.com>
2020-10-19 07:52:45 +0800

    Update auto_update_test.sh

--- auto_update_test.sh
+++ auto_update_test.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 cd /mnt/hgfs/Development/Odoo/odoo_test_modules
-if git checkout master &&
-    git fetch origin master &&
+if git fetch origin master &&
     [ `git rev-list HEAD...origin/master --count` != 0 ] &&
     git merge origin/master
 then
