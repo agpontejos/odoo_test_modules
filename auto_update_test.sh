3edefa1e5f98bbf22cbf01c953c3aad5a683e6ab
agpontejos <agpontejos@gmail.com>
2020-10-19 07:50:43 +0800

    Update auto_update_test.sh

--- auto_update_test.sh
+++ auto_update_test.sh
@@ -6,6 +6,8 @@ if git checkout master &&
     git merge origin/master
 then
     echo 'Updated!'
+    git pull
+    # su odoo11 -c "python3 /opt/odoo11/odoo11-server/odoo-bin -c /etc/odoo11-server.conf -d test_auto_update -u all --stop-after-init"
 else
     echo 'Not updated.'
 fi
