e3c0452b4a1901b0482b6b27751bab71f17c0d5c
agpontejos <agpontejos@gmail.com>
2020-10-19 14:02:16 +0800

    update

--- auto_update_test/models/models.py
+++ auto_update_test/models/models.py
@@ -5,7 +5,7 @@ from odoo import models, fields, api
 class auto_update_test(models.Model):
     _name = 'auto_update_test.auto_update_test'
 
-    name = fields.Char(string="Test1")
+    name = fields.Char(string="Test3")
     # value = fields.Integer()
     # value2 = fields.Float(compute="_value_pc", store=True)
     # description = fields.Text()
