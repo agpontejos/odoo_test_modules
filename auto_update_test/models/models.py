25c8f796c5d9ad942e20f96391953e94e2f7ec83
agpontejos <agpontejos@gmail.com>
2020-10-19 09:13:55 +0800

    update

--- auto_update_test/models/models.py
+++ auto_update_test/models/models.py
@@ -6,10 +6,10 @@ class auto_update_test(models.Model):
     _name = 'auto_update_test.auto_update_test'
 
     name = fields.Char("Test1")
-    value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
-    description = fields.Text()
+    # value = fields.Integer()
+    # value2 = fields.Float(compute="_value_pc", store=True)
+    # description = fields.Text()
 
-    @api.depends('value')
-    def _value_pc(self):
-        self.value2 = float(self.value) / 100
+    # @api.depends('value')
+    # def _value_pc(self):
+    #     self.value2 = float(self.value) / 100
