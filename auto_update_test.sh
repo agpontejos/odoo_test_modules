e8ebcf225849c222b89e386c93c114f39cb84f03
agpontejos <agpontejos@gmail.com>
2020-10-19 09:28:39 +0800

    Update auto_update_test.sh

--- auto_update_test.sh
+++ auto_update_test.sh
@@ -4,10 +4,10 @@ if git fetch origin 11.0 &&
     [ `git rev-list HEAD...origin/11.0 --count` != 0 ] &&
     git merge origin/11.0
 then
-    echo 'Updated!'
+    echo 'Initiating Update'
     git pull
     su odoo11 -c "python3 /opt/odoo11/odoo11-server/odoo-bin -c /etc/odoo11-server.conf -d test_auto_update -u all --stop-after-init"
     service odoo11-server restart
 else
-    echo 'Not updated.'
+    echo 'Already Upto Date'
 fi
