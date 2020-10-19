f1332d068ebfeb7cf750e4d5fa745d2eee9950d9
agpontejos <agpontejos@gmail.com>
2020-10-19 09:56:44 +0800

    Update update_odoo.sh

--- update_odoo.sh
+++ update_odoo.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 su odoo11 -c "python3 /opt/odoo11/odoo11-server/odoo-bin -c /etc/odoo11-server.conf -d test_auto_update -u all --stop-after-init --no-http"
+pkill -f odoo11
 service odoo11-server restart
